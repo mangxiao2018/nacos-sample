b23246f21ecf21497d051b8779e5214e750fe2aa
mangxiao <mangxiao2018@126.com>
2021-04-19 12:22:14 +0800

    service2

---
+++ nacos/service2/src/main/java/com/mangxiao/nacos/service2/Service2Bootstrap.java
@@ -0,0 +1,12 @@
+package com.mangxiao.nacos.service2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service2Bootstrap {
+
+    public static void main(String[] args){
+        SpringApplication.run(Service2Bootstrap.class, args);
+    }
+}
